9aeac004809c4bf962ec59dd78519f58e1b1f395
jhonvc <rayan_8020@hotmail.es>
2019-12-11 02:02:11 -0500

    parte diseño 2 Terminada

---
+++ app/src/main/java/com/example/hardcarry/LoginActivity.java
@@ -0,0 +1,38 @@
+package com.example.hardcarry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText etNick;
+    Button btnStart;
+    String nick;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        etNick=findViewById(R.id.txt_usuario);
+        btnStart=findViewById(R.id.btn_login);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nick=etNick.getText().toString();
+                if (nick.isEmpty()){
+                    etNick.setError("El nombre de usuario es obligatorio");
+
+                }else {
+                    Intent intent=new Intent(LoginActivity.this,GameActivity.class);
+                    intent.putExtra("nick",nick);
+                    startActivity(intent);
+                }
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/hardcarry/common/Cosntantes.java
@@ -0,0 +1,5 @@
+package com.example.hardcarry.common;
+
+public class Cosntantes {
+    public static final String ESTRA_NICK="nickname";
+}
